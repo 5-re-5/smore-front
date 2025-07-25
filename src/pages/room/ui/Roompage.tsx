f39c9d65767b4d40439266791f9dbea86fabd81e
terry <kdjtiger@naver.com>
2025-07-25 17:37:51 +0900

    feat: 마이크 및 마이크 감도 바 추가

--- src/pages/room/ui/Roompage.tsx
+++ src/pages/room/ui/Roompage.tsx
@@ -1,9 +1,10 @@
 import { useLocalTracks } from '@/entities/track/model/useLocalTracks';
 import LocalVideoTile from '@/widgets/local-video/ui/LocalVideoTile';
+import MicWaveform from '@/widgets/mic-visualizer/ui/MicWaveform';
 import { useState } from 'react';
 
 function Roompage() {
-  const { videoTrack } = useLocalTracks();
+  const { videoTrack, audioTrack } = useLocalTracks();
   // 유저 구분하기 위한 값
   const [participantName, setParticipantName] = useState(
     'Participant' + Math.floor(Math.random() * 100),
@@ -19,10 +20,13 @@ function Roompage() {
         버튼
       </button>
       {videoTrack ? (
-        <LocalVideoTile
-          track={videoTrack}
-          participantIdentity={participantName}
-        />
+        <>
+          <LocalVideoTile
+            track={videoTrack}
+            participantIdentity={participantName}
+          />
+          <MicWaveform track={audioTrack} />
+        </>
       ) : (
         <div>카메라 로딩중</div>
       )}
