76528ad1bc2ac38c8d97857afe7bfe43f6bb0ed7
terry <kdjtiger@naver.com>
2025-07-25 15:58:20 +0900

    fix: 카메라 무한 렌더링 fix

--- src/entities/track/model/useLocalTracks.ts
+++ src/entities/track/model/useLocalTracks.ts
@@ -31,7 +31,7 @@ export function useLocalTracks() {
       videoTrack?.stop();
       audioTrack?.stop();
     };
-  }, [videoTrack, audioTrack]);
+  }, []);
 
   return { videoTrack, audioTrack };
 }
