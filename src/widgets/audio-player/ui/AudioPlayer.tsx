2ce8b450074e6d450fddef8bbec8eda3f6571df4
Terry <kdjtiger@gmail.com>
2025-08-03 23:56:19 +0900

    style: 필요없는 파일 삭제

--- src/widgets/audio-player/ui/AudioPlayer.tsx
+++
@@ -1,7 +0,0 @@
-import { useAttachAudioTrack } from '../model/useAttachAudioTrack';
-import type { AudioPlayerProps } from '../types';
-
-export default function AudioPlayer({ track }: AudioPlayerProps) {
-  const audioRef = useAttachAudioTrack({ track });
-  return <audio ref={audioRef} id={track.sid} />;
-}
