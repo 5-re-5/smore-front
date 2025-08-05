1aa5e117b4f3c68f7eb7b21a95869fb1e953fe2a
terry <kdjtiger@naver.com>
2025-08-05 15:08:23 +0900

    style: 파일, 폴더 명 수정

--- src/pages/homepage/route.ts
+++ src/pages/homepage/route.ts
@@ -1,9 +1,9 @@
 import { createRoute } from '@tanstack/react-router';
 import { rootRoute } from '@/app/routing/__root';
-import Homepage from './ui/Homepage';
+import HomePage from './ui/Homepage';
 
 export const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
-  component: Homepage,
+  component: HomePage,
 });
