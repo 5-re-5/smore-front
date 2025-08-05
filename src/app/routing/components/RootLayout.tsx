d81a01d09ac8c9a6be5fcb169469a9252e13c3a1
terry <kdjtiger@naver.com>
2025-08-05 10:33:25 +0900

    feat: 푸터 아래로

--- src/app/routing/components/RootLayout.tsx
+++ src/app/routing/components/RootLayout.tsx
@@ -12,10 +12,12 @@ export const RootLayout = () => {
   );
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       {!shouldHideHeaderFooter && <Header />}
-      <Outlet />
+      <main className="flex-1">
+        <Outlet />
+      </main>
       {!shouldHideHeaderFooter && <Footer />}
-    </>
+    </div>
   );
 };
