88cfc22f2efce488182fb797836988793d551705
Terry <kdjtiger@gmail.com>
2025-08-07 01:12:36 +0900

    fix: build router 오류 제거

--- src/routes/__root.ts
+++
@@ -1,8 +0,0 @@
-// src/routes/__root.ts
-import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-
-export const rootRoute = createRootRouteWithContext<{
-  queryClient: unknown
-}>()({
-  component: () => Outlet,
-})
