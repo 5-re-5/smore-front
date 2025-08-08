c5be947d1210ea5e7b60a84f4f777ab4444c2ca8
terry <kdjtiger@naver.com>
2025-08-08 15:43:30 +0900

    fix: roomLeave API post 및 endPoint 변경

--- src/entities/room/api/leaveRoom.ts
+++ src/entities/room/api/leaveRoom.ts
@@ -2,8 +2,8 @@ import { request, REQUEST_METHOD } from '@/shared/api/request';
 
 export const leaveRoom = async (roomId: number, userId: number) => {
   const response = await request({
-    method: REQUEST_METHOD.DELETE,
-    url: `${import.meta.env.VITE_BACK_URL}/api/v1/study-rooms/${roomId}/participants?userId=${userId}`,
+    method: REQUEST_METHOD.POST,
+    url: `${import.meta.env.VITE_BACK_URL}/api/v1/study-rooms/${roomId}/leave?userId=${userId}`,
   });
 
   return response.data;
