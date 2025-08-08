5624e96e5f5ab4b5187458cc4a8c512304d80fbf
terry <kdjtiger@naver.com>
2025-08-08 15:31:55 +0900

    feat: 최근 스터디 api adapter 추가 & 방 나가기 api 추가

---
+++ src/entities/room/api/leaveRoom.ts
@@ -0,0 +1,10 @@
+import { request, REQUEST_METHOD } from '@/shared/api/request';
+
+export const leaveRoom = async (roomId: number, userId: number) => {
+  const response = await request({
+    method: REQUEST_METHOD.DELETE,
+    url: `${import.meta.env.VITE_BACK_URL}/api/v1/study-rooms/${roomId}/participants?userId=${userId}`,
+  });
+
+  return response.data;
+};
