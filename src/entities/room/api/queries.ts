760aa76134f8520e8ab83c6b93b53d747fdd6c0b
terry <kdjtiger@naver.com>
2025-08-12 11:42:35 +0900

    style: 주석 코드 제거

--- src/entities/room/api/queries.ts
+++ src/entities/room/api/queries.ts
@@ -1,10 +1,9 @@
 import { useQuery, useMutation } from '@tanstack/react-query';
-import { getRoomInfo, getRooms, verifyRoomPassword } from './room';
+import { getRoomInfo } from './room';
 import { joinRoom } from './joinRoom';
 import { rejoinRoom } from './rejoinRoom';
 import { leaveRoom } from './leaveRoom';
 import { useRoomTokenStore } from '../model/useRoomTokenStore';
-import type { RoomListParams } from './type';
 import { roomQueryKeys } from './queryKeys';
 
 // Room 상세 조회
@@ -13,22 +12,6 @@ export const useRoomInfoQuery = (roomId: number) => {
     queryKey: [roomQueryKeys.ROOM, roomId],
     queryFn: () => getRoomInfo(roomId),
     enabled: !!roomId,
-  });
-};
-
-// Room 목록 조회
-export const useRoomListQuery = (params?: RoomListParams) => {
-  return useQuery({
-    queryKey: [roomQueryKeys.ROOM_LIST, params],
-    queryFn: () => getRooms(params),
-  });
-};
-
-// Room 비밀번호 검증
-export const useVerifyRoomPasswordMutation = () => {
-  return useMutation({
-    mutationFn: ({ roomId, password }: { roomId: number; password: string }) =>
-      verifyRoomPassword(roomId, password),
   });
 };
 
